545d1957d2ceb83050a87a5a5b649422f9eb6161
DavizinS <93940192+DavizinS@users.noreply.github.com>
2022-03-11 14:50:54 -0300

    correção 0.5

--- code/script.js
+++ code/script.js
@@ -1,12 +1,31 @@
+/* Variaveis Lampada */
 let fundoq = document.getElementById('int1')
+let statusl = document.getElementById('status')
+let botao = document.getElementById('botaol')
+let res = document.getElementById('cliques')
 let ligado = false
+let cliques = 0
+/* --------------------- */
 
 function onoff() {
+    cliques += 1
     if (ligado == false)  {
-        ligado = true
-        fundoq.style.background = '#ffffff'
+        ligado = true;
+        statusl.innerHTML = 'ACENDEU';
+        botao.value = 'Desligar';
+        fundoq.style.background = '#ffffff';
+
     } else if (ligado == true) {
-        ligado = false
-        fundoq.style.background = '#000000'
+        ligado = false;
+        fundoq.style.background = '#000000';
+        botao.value = 'Ligar';
+        statusl.innerHTML = 'APAGOU';
+    }
+    if (cliques == 10) {
+        window.alert('Ta marolando kkkkk já clicou 10x');
+    } else if (cliques == 50) {
+        window.alert('Coe, kkkkjs já clicou 50x mano, para');
+    } else if (cliques == 100) {
+        window.alert('Na moral, vou te tirar do site kkkkk 100 VEZES JÁ!')
     }
 }
